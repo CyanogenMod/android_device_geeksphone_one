6b16c3d3861f02a2bad2e5e21d77f912e43c4a5e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2010-12-22 21:06:25 +0000

    gralloc: Add LOCAL_MODULE_TAGS
    Change-Id: I7a6fdfdcf9389dd3075a58262a550cf6f1e1c838

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -23,6 +23,8 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 
+LOCAL_MODULE_TAGS := optional
+
 LOCAL_SRC_FILES := 	\
 	allocator.cpp 	\
 	gralloc.cpp 	\
