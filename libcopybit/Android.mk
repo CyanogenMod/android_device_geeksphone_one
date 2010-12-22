2956d418b31d5f6a7902ede94f0ac62aae7987fc
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2010-12-22 21:05:52 +0000

    copybit: Updates for gingerbread
    Change-Id: I91d7dc203b27c648c84d25fd05cddb013f764757

--- libcopybit/Android.mk
+++ libcopybit/Android.mk
@@ -19,7 +19,9 @@ LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 
+
 include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog
